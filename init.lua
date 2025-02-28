a7af5032b88c837f76e2346ebeb09e78ed05230e
luca0s <luca@schluer.eu>
2025-02-28 17:41:05 +0100

    added nvimcolorizer

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require("config.lazy")
-require("config.remap")
 require("config.set")
+require("config.remap")
 require("config.autocmd")
+require("config.lazy")
 require ('nvim-treesitter.install').compilers = { "clang", "gcc" }
 SetColor("vague")
