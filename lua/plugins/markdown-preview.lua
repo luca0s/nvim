b755b7aef4fe780caa533bf6a4527afe6e886f71
luca@schluer.eu <luca@schluer.eu>
2025-03-16 01:20:21 +0100

    added a md viewer

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    config = function ()
+        vim.keymap.set("n", "<leader>mdn", ":MarkdownPreview<CR>")
+        vim.keymap.set("n", "<leader>mds", ":MarkdownPreviewStop<CR>")
+    end
+}
