8fab1b55bf24b7cc0493ef8d192ea0cd07914da9
Luca Schluer <luca@schluer.eu>
2025-05-10 01:23:28 +0200

    repacled lsp setup with blink and conform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,41 +1,13 @@
 return {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
     dependencies = {
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
+        "neovim/nvim-lspconfig",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
     },
+
     config = function()
-        -- Add cmp_nvim_lsp capabilities settings to lspconfig
-        -- This should be executed before you configure any language server
-        local lspconfig_defaults = require('lspconfig').util.default_config
-        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-            'force',
-            lspconfig_defaults.capabilities,
-            require('cmp_nvim_lsp').default_capabilities()
-        )
-
-        -- This is where you enable features that only work
-        -- if there is a language server active in the file
-        vim.api.nvim_create_autocmd('LspAttach', {
-            desc = 'LSP actions',
-            callback = function(event)
-                local opts = {buffer = event.buf}
-
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-            end,
-        })
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
 
         -- Use Mason to install and setup language servers
         require('mason').setup({})
@@ -45,19 +17,11 @@ return {
             ensure_installed = {'lua_ls', 'rust_analyzer'},
             handlers = {
                 function(server_name)
-                    require('lspconfig')[server_name].setup({})
+                    require('lspconfig')[server_name].setup({ capabilities = capabilities })
                 end,
 
                 lua_ls = function()
                     require("lspconfig").lua_ls.setup({
-                    --     settings = {
-                    --         Lua = {
-                    --             runtime = { version = "Lua 5.1" },
-                    --             diagnostics = {
-                    --                 globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                    --             }
-                    --         }
-                    --     }
                         on_init = function(client)
                             if client.workspace_folders then
                                 local path = client.workspace_folders[1].name
@@ -68,21 +32,13 @@ return {
 
                             client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
                                 runtime = {
-                                    -- Tell the language server which version of Lua you're using
-                                    -- (most likely LuaJIT in the case of Neovim)
                                     version = 'LuaJIT'
                                 },
-                                -- Make the server aware of Neovim runtime files
                                 workspace = {
                                     checkThirdParty = false,
                                     library = {
                                         vim.env.VIMRUNTIME
-                                        -- Depending on the usage, you might want to add additional paths here.
-                                        -- "${3rd}/luv/library"
-                                        -- "${3rd}/busted/library",
                                     }
-                                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                                    -- library = vim.api.nvim_get_runtime_file("", true)
                                 }
                             })
                         end,
@@ -92,24 +48,6 @@ return {
                     })
                 end
             },
-        })
-
-        -- This sets up code autocompletion
-        local cmp = require('cmp')
-        cmp.setup({
-            sources = {
-                {name = 'nvim_lsp'},
-            },
-            snippet = {
-                expand = function(args)
-                    -- You need Neovim v0.10 to use vim.snippet
-                    vim.snippet.expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
         })
     end
 }
