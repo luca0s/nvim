052e984822a3d60314fd82e8df55189ca89b7f73
Luca Schluer <luca@schluer.eu>
2025-07-15 21:46:09 +0200

    changed the way to install formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,28 @@
 return {
 	"stevearc/conform.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 	opts = {},
 	config = function()
+		local formatters_by_ft = {
+			lua = { "stylua" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt", lsp_format = "fallback" },
+			python = { "black", lsp_format = "fallback" },
+			-- Conform will run the first available formatter
+			javascript = { "prettier", stop_after_first = true },
+		}
+
+		local ensure_installed = {}
+		for _, name in pairs(formatters_by_ft) do
+			table.insert(ensure_installed, name[1])
+		end
+
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
-				python = { "black", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				nix = { "alejandra", lsp_format = "fallback" },
-			},
+			formatters_by_ft = formatters_by_ft,
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
