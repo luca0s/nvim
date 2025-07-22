69bdb658fc0d974d46e85e2bd05d3485c47145c9
Luca Schluer <luca@schluer.eu>
2025-07-22 18:12:50 +0200

    changed file finding behaviour

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,19 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    config = function()
-        require("telescope").setup({})
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	config = function()
+		require("telescope").setup({})
 
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fm", builtin.marks, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", function()
+			local success, _ = pcall(builtin.git_files)
+			if not success then
+				builtin.find_files()
+			end
+		end, {})
+		vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fm", builtin.marks, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+	end,
 }
